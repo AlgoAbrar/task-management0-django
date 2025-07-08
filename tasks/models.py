9363b3250d0b2e53c2e5cdc0207c9d5691f8483c
“Saiyedul <saiyedul.abrar1430@gmail.com>
2025-07-08 22:41:36 +0600

    added

---
+++ tasks/models.py
@@ -0,0 +1,86 @@
+from django.db import models
+from django.core.mail import *
+from django.db.models.signals import *
+from django.dispatch import *
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('COMPLETED', 'Completed')
+    ]
+    project = models.ForeignKey(
+        "Project",
+        on_delete=models.CASCADE,
+        default=1
+    )
+    assigned_to = models.ManyToManyField(Employee, related_name='tasks')
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    due_date = models.DateField()
+    status = models.CharField(
+        max_length=15, choices=STATUS_CHOICES, default="PENDING")
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # details
+
+    def __str__(self):
+        return self.title
+
+
+class TaskDetail(models.Model):
+    HIGH = 'H'
+    MEDIUM = 'M'
+    LOW = 'L'
+    PRIORITY_OPTIONS = (
+        (HIGH, 'High'),
+        (MEDIUM, 'Medium'),
+        (LOW, 'Low')
+    )
+    task = models.OneToOneField(
+        Task,
+        on_delete=models.CASCADE,
+        related_name='details',
+    )
+    # assigned_to = models.CharField(max_length=100)
+    priority = models.CharField(
+        max_length=1, choices=PRIORITY_OPTIONS, default=LOW)
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Details form Task {self.task.title}"
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    start_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
+#signals
+
+
+
+@receiver(m2m_changed, sender=Task.assigned_to.through)
+def notify_employees_on_task_creation(sender,instance, action ,**kwargs):
+        if action=='post_add':
+            assigned_emails=[emp.email for emp in instance.assigned_to.all()]
+            
+            send_mail(
+                "New Task Assigned",
+                f"You have been assigned with a new task: {instance.title}",
+                "saiyedul.abrar1430@gmail.com",
+                assigned_emails
+            )
